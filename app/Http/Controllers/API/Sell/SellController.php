eff8434515cd4df178b7acb367462fe31ccf3b12
Sakil <shakilreyan9397@gmail.com>
2023-01-28 16:33:52 +0600

    discount added

--- app/Http/Controllers/API/Sell/SellController.php
+++ app/Http/Controllers/API/Sell/SellController.php
@@ -78,6 +78,7 @@ class SellController extends Controller
                 'total_quantity'=>$request->total_quantity,
                 'total_amount'=>$request->total_amount,
                 'total_paid_amount'=>$request->total_paid_amount,
+                'total_discount_amount'=>$request->total_discount_amount,
                 'total_due_amount'=>$request->total_due_amount,
             ];
             $sell_id = Sell::insertGetId($purchase);
